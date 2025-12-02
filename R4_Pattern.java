b0e194a42feaf7f91e3307804b9e7aed7193c8c9
priyanka-k1234 <priayankak866@gmail.com>
2025-12-02 15:06:29 +0530

    Add files via upload

---
+++ R4_Pattern.java
@@ -0,0 +1,28 @@
+package Patterns;
+
+public class R4_Pattern {
+
+	public static void main(String[] args) {
+		
+		    
+
+		        int n = 3;
+		        int nst = 1;
+		        int row = 1;
+
+		        while (row <= n) {
+		            for (int i = 0; i < nst; i++) {
+		                System.out.print("*");
+		            }
+
+		            System.out.println();
+		            row++;
+		            nst++;
+		        }
+		    }
+		
+
+
+	
+
+}
